ea55c6cb0414518df740879b06a574be6e667026
Demian Vikovych <demvikovych@gmail.com>
2024-10-11 12:19:57 +0200

    section 3 completed

---
+++ 02-TypesAndVariables/3-3.py
@@ -0,0 +1,11 @@
+x = 7
+y = 34
+z = 0 # additional, auxiliary variable
+print("Before swapping: x=", x, "y=", y)
+z+=x
+x1 =(z-x)+y
+y1=z
+# swap the values
+
+
+print("After swapping: x=", x1, "y=", y1)
---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,14 @@
+#The distance to the horizon depends on the height of the observer above the ground. The higher you are, the farther away the horizon is. For most situations, you can use the following formula:
+import math
+h= 20
+d = 3.57 * math.sqrt(h)
+print(d)
+
+#Where:
+
+#d is the distance to the horizon in kilometers
+#h is the height of the observer in meters
+#Write a program that calculates the distance to the horizon from a height given in meters from the keyboard. Then, using the program, calculate the distance to the horizon in km when:
+
+#You are standing on a beach, by the sea, on the water line (calculate the distance for your height in m). You have probably been to the seaside many times. Can you believe that the horizon is only a few kilometers away?
+#You are looking out of a hotel window located by the sea, the window is at a height of 20 meters.
